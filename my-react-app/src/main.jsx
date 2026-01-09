36d3b76fb6728937d0ca318d5291d0bd74f984d5
ReggeMeister <mattiaswaern04@gmail.com>
2026-01-09 22:20:15 +0100

    commit

---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,31 @@
+import { createRoot } from 'react-dom/client';
+
+
+
+class Car {
+  constructor(name){
+    this.brand = name;
+  }
+
+  present(){
+    return 'I have a ' + this.brand;
+  }
+}
+
+class Model extends Car {
+  constructor(name, mod){
+    super(name);
+    this.model = mod;
+  }
+  show(){
+    return this.present() + ', it is a ' + this.model
+  }
+}
+
+const mycar = new Model("Ford", "Mustang");
+mycar.show();
+
+
+createRoot(document.getElementById('annika')).render(
+  mycar.show()
+);
